2f51af75959fcd02b3c7c3f4a440216f27d0b742
aarongustafson <aaron@easy-designs.net>
2011-10-08 17:15:39 -0400

    updated the styles include and added back in the compression

--- templates/styles/.html.php
+++ templates/styles/.html.php
@@ -1,7 +1,7 @@
-  <link rel="stylesheet" type="text/css" href="/css/main.css?{exp:template_updated_on template='styles/main'}"/>
+  <link rel="stylesheet" type="text/css" href="/css/main.css?20111008"/>
   
   <!--[if lte IE 8]><script src="/js/respond.js"></script><![endif]-->
-  <!--[if IE 8]><link rel="stylesheet" type="text/css" media="all" href="/css/ie8.css?{exp:template_updated_on template='styles/ie8'}"/><![endif]-->
-  <!--[if lte IE 7]><link rel="stylesheet" type="text/css" media="all" href="/css/ie7.css?{exp:template_updated_on template='styles/ie7'}"/><![endif]-->
-  <!--[if lte IE 6]><link rel="stylesheet" type="text/css" media="all" href="/css/ie6.css?{exp:template_updated_on template='styles/ie6'}"/><![endif]-->  
+  <!--[if IE 8]><link rel="stylesheet" type="text/css" media="all" href="/css/ie8.css?20111008"/><![endif]-->
+  <!--[if lte IE 7]><link rel="stylesheet" type="text/css" media="all" href="/css/ie7.css?20111008"/><![endif]-->
+  <!--[if lte IE 6]><link rel="stylesheet" type="text/css" media="all" href="/css/ie6.css?20111008"/><![endif]-->  
   {exp:gist:css media="all"}
