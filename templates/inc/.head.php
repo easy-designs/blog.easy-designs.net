db3a8d8d5df3178dcd4c806615b10aebd8b7b309
aarongustafson <aaron@easy-designs.net>
2011-10-10 11:29:03 -0400

    Forgot to include the image

--- templates/inc/.head.php
+++ templates/inc/.head.php
@@ -11,7 +11,7 @@
 	<meta property="og:site_name" content="{site_name}" />
 	<meta property="og:title" content="{embed:title}" />
 	<meta property="og:type" content="article" />
-	<meta property="og:image" content="" />
+	<meta property="og:image" content="/img/facebook-icon.png" />
 	<meta property="og:url" content="{embed:canonical_url}" />
 	<meta property="og:description" content="{embed:description}" />
 	<meta property="fb:admins" content="aaronmgustafson,1010367716,117286581705101" />
