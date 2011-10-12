b80b68f4258c71e8967d59a081b768c469750166
aarongustafson <aaron@easy-designs.net>
2011-10-12 12:29:21 -0400

    a few tweaks in the Javascript

--- templates/javascripts/main.php
+++ templates/javascripts/main.php
@@ -8,7 +8,7 @@ License:				MIT License (see homepage)
 Note:					 If you change or improve on this script, please let us know by
 								emailing the author (above) with a link to your demo page.
 ------------------------------------------------------------------------------*/
-(function(){var FunctionHandler={version:"0.2"},pages={};function initialize(){var body_id=$("body").attr("id");if(body_id!=false&&typeof(pages[body_id])!="undefined"){run(pages[body_id])}if(typeof(pages["*"])!="undefined"){run(pages["*"])}}$(document).ready(initialize);FunctionHandler.register=function(id,callback){if((typeof(id)!="string"&&!(id instanceof Array))||typeof(callback)!="function"){return false}if(typeof(id)=="string"&&id.indexOf(", ")!=-1){id=id.split(", ")}if(id instanceof Array){for(var i=id.length-1;i>=0;i--){add(id[i],callback)}}else{add(id,callback)}return true};function add(id,callback){if(typeof(pages[id])=="undefined"){pages[id]=[]}pages[id].push(callback)}function run(arr){if(!(arr instanceof Array)){return}for(var i=arr.length-1;i>=0;i--){arr[i]()}}window.FunctionHandler=FunctionHandler})();
+(function($){var FunctionHandler={version:"0.2"},pages={};function initialize(){var body_id=$("body").attr("id");if(body_id!=false&&typeof(pages[body_id])!="undefined"){run(pages[body_id])}if(typeof(pages["*"])!="undefined"){run(pages["*"])}}$(document).ready(initialize);FunctionHandler.register=function(id,callback){if((typeof(id)!="string"&&!(id instanceof Array))||typeof(callback)!="function"){return false}if(typeof(id)=="string"&&id.indexOf(", ")!=-1){id=id.split(", ")}if(id instanceof Array){for(var i=id.length-1;i>=0;i--){add(id[i],callback)}}else{add(id,callback)}return true};function add(id,callback){if(typeof(pages[id])=="undefined"){pages[id]=[]}pages[id].push(callback)}function run(arr){if(!(arr instanceof Array)){return}for(var i=arr.length-1;i>=0;i--){arr[i]()}}window.FunctionHandler=FunctionHandler})(jQuery);
 
 /*
  * anchor-include pattern for already-functional links that work as a client-side include
@@ -39,113 +39,123 @@ jQuery.getScripts = function(scripts, onComplete)
 	};
 	while ( s )
 	{
-		$.getScript( scripts[s-1], onScriptLoaded );
+		jQuery.getScript( scripts[s-1], onScriptLoaded );
 		s--;
 	}
 };
 
-FunctionHandler.register(
-	'*',
-	function()
-	{
-		
-		// Typekit
-		$.getScript('http://use.typekit.com/tjo5hgd.js',function(){
-			try {
-				Typekit.load();
-			} catch( e ) { }
-		});
+(function($){
 	
-		// Google Analytics
-		window._gaq = window._gaq || [];
-		window._gaq.push(['_setAccount', 'UA-176472-7']);
-		window._gaq.push(['_trackPageview']);
+	FunctionHandler.register(
+		'*',
+		function()
+		{
 
-		(function() {
-			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		})();
-		
-	});
+			// for iOS (remove the toolbar)
+			if ( window.location.hash == '' )
+			{
+	  			window.scrollTo(0, 1);
+			}
 
-FunctionHandler.register(
-	'single-entry',
-	function()
-	{
-		$('body')
-			.bind( 'ajaxInclude', function(){
-				
-				// only if a large screen
-				if ( $(window).width() > 700 )
-				{
-					// only run the form thing once
-					$(this).delegate('#comment_form input, #comment_form textarea','focus',function setup(){
-						// remove this delegation
-						$('body').undelegate('#comment_form input, #comment_form textarea');
-						// load the stuff
-						$.getScript('/js/jquery.textile.js',function(){
-							var
-							$comments	= $('#comments .hfeed'),
-							$name		= $('#comments-name'),
-							$text		= $('#comments-comment'),
-							$preview	= $('<li class="preview hentry">' +
-											'<div class="entry-content"></div>' + // this needed to be a div for IE
-											'<ul class="meta"><li>' + 
-											'<img class="photo" src="/img/gravitar.png" alt=""/><span></span>' + 
-											'</li><li>This is a preview</li></ul>' + 
-											'</li>').hide(),
-							$comment	= $preview.find('.entry-content');
-							function updatePreview()
-							{
-								if ( $text.val() )
-								{
-									if ( $preview.is(':hidden') )
-									{
-										if ( $comments.length < 1 )
-										{
-											$comments = $('<ol class="hfeed" id="comments"></ol>');
-											$('#no-comments').replaceWith($comments);
-										}
-										$preview.appendTo($comments)
-														.animate({height:'show',opacity:'show'},'fast');
-									}
-									updatePreview = function()
-									{
-										if ( $name.val() )
-										{
-											$preview.find('.meta span').text($name.val())
-										}
-										$comment.html( $.textile( $text.val() ) );
-									};
-									updatePreview();
-								}
-							}
-							updatePreview();
-							$name.bind('keyup blur change',updatePreview);
-							$text.bind('keyup blur change',updatePreview);
-						});
-					});
-					
-					$('#bookmark a').click(function(e){
-						e.preventDefault();
-						window.open(this.href,'share-this','height=300,width=500,status=no,toolbar=no');
-					});
-				}
-				
+			// Typekit
+			$.getScript('http://use.typekit.com/tjo5hgd.js',function(){
+				try {
+					Typekit.load();
+				} catch( e ) { }
 			});
-		
-		
-		$("[data-append],[data-replace],[data-after],[data-before]")
-			.ajaxInclude();
 
-		
-		
-		//if ( $(window).width() >  )
-		// Google+
-		//(function() {
-		//	var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-		//	po.src = 'https://apis.google.com/js/plusone.js';
-		//	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-		//})();
-	});
+			// Google Analytics
+			window._gaq = window._gaq || [];
+			window._gaq.push(['_setAccount', 'UA-176472-7']);
+			window._gaq.push(['_trackPageview']);
+
+			(function() {
+				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
+
+		});
+
+	FunctionHandler.register(
+		'single-entry',
+		function()
+		{
+			$('body')
+				.bind( 'ajaxInclude', function(){
+
+					// only if a large screen
+					if ( $(window).width() > 700 )
+					{
+						// only run the form thing once
+						$(this).delegate('#comment_form input, #comment_form textarea','focus',function setup(){
+							// remove this delegation
+							$('body').undelegate('#comment_form input, #comment_form textarea');
+							// load the stuff
+							$.getScript('/js/jquery.textile.js',function(){
+								var
+								$comments	= $('#comments .hfeed'),
+								$name		= $('#comments-name'),
+								$text		= $('#comments-comment'),
+								$preview	= $('<li class="preview hentry">' +
+												'<div class="entry-content"></div>' + // this needed to be a div for IE
+												'<ul class="meta"><li>' + 
+												'<img class="photo" src="/img/gravitar.png" alt=""/><span></span>' + 
+												'</li><li>This is a preview</li></ul>' + 
+												'</li>').hide(),
+								$comment	= $preview.find('.entry-content');
+								function updatePreview()
+								{
+									if ( $text.val() )
+									{
+										if ( $preview.is(':hidden') )
+										{
+											if ( $comments.length < 1 )
+											{
+												$comments = $('<ol class="hfeed" id="comments"></ol>');
+												$('#no-comments').replaceWith($comments);
+											}
+											$preview.appendTo($comments)
+															.animate({height:'show',opacity:'show'},'fast');
+										}
+										updatePreview = function()
+										{
+											if ( $name.val() )
+											{
+												$preview.find('.meta span').text($name.val())
+											}
+											$comment.html( $.textile( $text.val() ) );
+										};
+										updatePreview();
+									}
+								}
+								updatePreview();
+								$name.bind('keyup blur change',updatePreview);
+								$text.bind('keyup blur change',updatePreview);
+							});
+						});
+
+						$('#bookmark a').click(function(e){
+							e.preventDefault();
+							window.open(this.href,'share-this','height=300,width=500,status=no,toolbar=no');
+						});
+					}
+
+				});
+
+
+			$("[data-append],[data-replace],[data-after],[data-before]")
+				.ajaxInclude();
+
+
+
+			//if ( $(window).width() >  )
+			// Google+
+			//(function() {
+			//	var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+			//	po.src = 'https://apis.google.com/js/plusone.js';
+			//	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+			//})();
+		});
+			
+})(jQuery);
