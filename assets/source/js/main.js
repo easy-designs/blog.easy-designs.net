5f777a96fcf2450631c3000cd888213f1de143fc
aarongustafson <aaron@easy-designs.net>
2013-07-10 23:26:31 -0700

    re-compiling JavaScript

--- assets/source/js/main.js
+++ assets/source/js/main.js
@@ -94,17 +94,6 @@ jQuery.getScripts = function(scripts, onComplete)
 			$.getScript('http://use.typekit.com/tjo5hgd.js',function(){
 				try {Typekit.load();} catch( e ) {}});
 
-			// Google Analytics
-			//window._gaq = window._gaq || [];
-			//window._gaq.push(['_setAccount', 'UA-176472-7']);
-			//window._gaq.push(['_trackPageview']);
-            //
-			//(function() {
-			//	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-			//	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-			//	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-			//})();
-			
 			$("[data-append],[data-replace],[data-after],[data-before]")
 				.ajaxInclude();
 
