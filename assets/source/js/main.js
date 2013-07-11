fb1ada5e7f0ec37d962f21752189b748066aafc5
aarongustafson <aaron@easy-designs.net>
2013-07-11 14:19:06 -0700

    Non-JS fallback, but keeping the JS

--- assets/source/js/main.js
+++ assets/source/js/main.js
@@ -96,6 +96,17 @@ jQuery.getScripts = function(scripts, onComplete)
 
 			$("[data-append],[data-replace],[data-after],[data-before]")
 				.ajaxInclude();
+				
+			// Google Analytics
+			window._gaq = window._gaq || [];
+			window._gaq.push(['_setAccount', 'UA-176472-7']);
+			window._gaq.push(['_trackPageview']);
+			
+			(function() {
+				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
 
 		});
 
